2dca36178fcc26902cb9c2a8daeddfaf722d8599
Yiteng Ma <yitengma@umich.edu>
2024-01-20 00:44:11 -0500

    plotter update

--- plotter.py
+++ plotter.py
@@ -3,21 +3,23 @@ import scipy.io
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def load_csv(file_path):
     with open(file_path, mode='r') as file:
         reader = csv.reader(file)
 
         iterations = []
         ankle_angle = []
+        time = []
 
         for row in reader:
-
             iterations.append(int(row[0]))
+            time.append(float(row[1]))
             ankle_angle.append(float(row[2]))
-
-        return iterations, ankle_angle
+            
+        return iterations, time, ankle_angle
     
-def load_mat(file_path):
+def load_mat(file_path): 
     mat_data = scipy.io.loadmat(file_path)
 
     JIM_time = mat_data['output'][:,0]
@@ -26,18 +28,20 @@ def load_mat(file_path):
 
     return JIM_time, JIM_angle
 
+
 file_path_encoder = r'I:\My Drive\Neurobionics\ExoBoot\Data\20240118-171052_encoder_test_data_R.csv'
-encoder_i, encoder_angle = load_csv(file_path_encoder)
+encoder_i, encoder_time, encoder_angle = load_csv(file_path_encoder)
 
 file_path_JIM = r"I:\My Drive\Neurobionics\ExoBoot\Data\JIM\encoderchecktest2.mat"
 JIM_time, JIM_angle = load_mat(file_path_JIM)
+
 
 #Plot
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 8))
 
 # First subplot
-ax1.scatter(encoder_i, encoder_angle, label='Encoder Angle', color='blue')
-ax1.set_xlabel('Iteration')
+ax1.scatter(encoder_time, encoder_angle, label='Encoder Angle', color='blue')
+ax1.set_xlabel('Time')
 ax1.set_ylabel('Ankle Angle')
 ax1.grid(True)
 ax1.legend()
