3289acbe1b40d431f9aa44c9f2da5c90a7751baf
Yiteng Ma <myt011023@gmail.com>
2024-02-20 15:29:23 -0500

    test2 update

--- plotter.py
+++ plotter.py
@@ -158,19 +158,20 @@ def plot_piecewise_fit():
 
 def plot_controller_data():
     csv_file = "ExoBoot/data/basic_controller_spring_2.csv"
-    cal_file = "ExoBoot/data/basic_controller_spring_CAL_1.mat"
+    cal_file = "ExoBoot/data/basic_controller_spring_CAL_1_4.mat"
     mat_file = "ExoBoot/data/basic_controller_spring_EXO_2.mat"
 
     JIM_time, JIM_angle, JIM_torque =  load_mat(mat_file, cal_file, False, False)
     JIM_time_filt, JIM_angle_filt, JIM_torque_filt =  load_mat(mat_file, cal_file, False, True)
     controller_data = pd.read_csv(csv_file)
 
-    plt.figure(figsize=(8, 6))
+    plt.figure(figsize=(8, 6), dpi=100)
     # plt.scatter(JIM_angle, JIM_torque, label='JIM_torque', s=2)
     # plt.scatter(JIM_angle_filt, JIM_torque_filt, label='JIM_filtered', s=2)
     # plt.scatter(controller_data["ankle_angle"], controller_data["commanded_torque"], label='command', s=2)
     # plt.scatter(controller_data["ankle_angle"], controller_data["passive_torque"], label='passive', s=2)
     # plt.scatter(controller_data["ankle_angle"], controller_data["commanded_torque"] + controller_data["passive_torque"], label='output', s=2)
+    
     # plt.scatter(JIM_time, JIM_torque, label='JIM_torque', s=2)
     # plt.scatter(JIM_time_filt, JIM_torque_filt, label='JIM_filtered', s=2)
     plt.scatter(controller_data["time"], controller_data["commanded_torque"], label='command', s=2)
