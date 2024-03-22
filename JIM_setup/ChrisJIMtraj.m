808356e985d74bb167ab91f1fce3e317d2649bf8
Yiteng Ma <myt011023@gmail.com>
2024-03-22 17:12:18 -0400

    traj update

--- JIM_setup/ChrisJIMtraj.m
+++ JIM_setup/ChrisJIMtraj.m
@@ -32,28 +32,30 @@ function [ pos, time ] = ChrisJIMtraj( trajno )
 
 
     elseif trajno == 1
-        traj_data_path = 'I:\My Drive\Locomotor\ExoBoot\JIM_setup\ankle_test_right_swing_112run1.csv';
-        % readtable(traj_data_path);
-        % trajectory.angle = ans.AnkleAngle;
-        % trajectory.time = ans.Time-ans.Time(1);
+        traj_data_path = '/Users/yitengma/Library/CloudStorage/GoogleDrive-yitengma@umich.edu/My Drive/Locomotor/ExoBoot/JIM_setup/ankle_test_right_swing_112run1.csv';
+
         traj_data = csvread(traj_data_path,1,0);
         traj_data_angle = -rad2deg(traj_data(:,7));
         traj_data_time = traj_data(:,1);
-        
+        traj_data_time = traj_data_time - traj_data_time(1);
+
         freq = 250;
         period = 1/freq;
         
         newtimes = linspace(traj_data_time(1), traj_data_time(end), round((traj_data_time(end)/period)+1));
-        newtimes = newtimes - traj_data_time(1);
-        newangles = interp1(traj_data_time, traj_data_angle, newtimes);
+        newangles = interp1(traj_data_time, traj_data_angle, newtimes, 'spline');
+
+        fc = 6;
+        order = 5;
+        [b, a] = butter(order, fc / (freq / 2), 'low');
+        filtered_angles = filtfilt(b, a, newangles);
         
-        pos = newangles;
+        pos = filtered_angles;
         trajectory.time = newtimes;
 
-        % figure
-        % plot(traj_data_time,traj_data_angle,'r')
-        % hold on
-        % plot(newtimes,pos,'b')
+        plot(traj_data_time(1:3000),traj_data_angle(1:3000),'r')
+        hold on
+        plot(newtimes(1:6000),pos(1:6000),'b')
 
 
     else
