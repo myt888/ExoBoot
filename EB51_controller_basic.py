22a5ea074f7e469db19280ecbb17fcc43f6dd4d3
Yiteng Ma <myt011023@gmail.com>
2024-03-28 16:58:06 -0400

    update

--- EB51_controller_basic.py
+++ EB51_controller_basic.py
@@ -73,7 +73,7 @@ class Controller():
 
             if not synced:
                 des_torque = 0
-                if abs(current_angle) > 1:
+                if abs(current_angle) > 0.5:
                     synced = True
                     print("Synced with JIM device. Start commanding torque.")
             else:
