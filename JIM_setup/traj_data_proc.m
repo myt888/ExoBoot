8272d5aa36d0cea1df58ae04c7f14bb65ede4759
Yiteng Ma <myt011023@gmail.com>
2024-03-29 16:20:46 -0400

    update

--- JIM_setup/traj_data_proc.m
+++ JIM_setup/traj_data_proc.m
@@ -1,4 +1,5 @@
-traj_data_path = "I:\My Drive\Locomotor\ExoBoot\JIM_setup\ankle_test_right_swing_112run1.csv";
+% traj_data_path = "I:\My Drive\Locomotor\ExoBoot\JIM_setup\ankle_test_right_swing_112run1.csv";
+traj_data_path = "ankle_test_right_swing_112run1.csv";
 traj_data = readtable(traj_data_path,'VariableNamingRule','modify');
 
 traj_data_angle = -rad2deg(traj_data.AnkleAngle);   % Reverse for JIM
@@ -27,12 +28,9 @@ filtered_angles = filtfilt(b, a, new_angles);
 filtered_torques = filtfilt(b, a, new_torque);
 
 figure
-plot(new_times(1:5000),new_torque(1:5000),'LineWidth',1)
-hold on
-plot(new_times(1:5000),filtered_torques(1:5000),'LineWidth',1)
-hold off
+plot(traj_data_time(1:10000),traj_data_angle(1:10000),'LineWidth',1)
 xlabel("time [s]")
-ylabel("torque [Nm]")
+ylabel("angle [deg]")
 
 figure
 plot(new_times(1:5000),new_torque(1:5000),'LineWidth',1)
@@ -51,5 +49,6 @@ xlabel("time [s]")
 ylabel("angle [deg]")
 
 output_data = table(new_times', filtered_angles', filtered_torques', 'VariableNames', {'time', 'ankle_angle', 'commanded_torque'});
-output_path = "I:\My Drive\Locomotor\ExoBoot\JIM_setup\traj_data_Katharine.csv";
+% output_path = "I:\My Drive\Locomotor\ExoBoot\JIM_setup\traj_data_Katharine.csv";
+output_path = "traj_data_Katharine.csv";
 writetable(output_data, output_path);
