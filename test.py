717b61b07541f0d14748c97eae9caa9ba26d5c07
Yiteng Ma <myt011023@gmail.com>
2024-04-02 15:16:40 -0400

    update

--- test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-traj_data = pd.read_csv(f'I:\\My Drive\\Locomotor\\ExoBoot\\JIM_setup\\traj_data_Katharine.csv')
-des_torque = traj_data['commanded_torque'][4799]
-print(des_torque)
-
-print(len(traj_data))
